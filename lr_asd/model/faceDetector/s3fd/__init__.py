3afc65df70c15d4047f226b73e8570249067e6b1
Ahmad Bashir <119527683+ahmad-bashir-ai@users.noreply.github.com>
2025-04-24 13:11:24 +0500

    Update __init__.py to add model weight path as argument

--- lr_asd/model/faceDetector/s3fd/__init__.py
+++ lr_asd/model/faceDetector/s3fd/__init__.py
@@ -6,25 +6,19 @@ from torchvision import transforms
 from .nets import S3FDNet
 from .box_utils import nms_
 
-PATH_WEIGHT = 'model/faceDetector/s3fd/sfd_face.pth'
-if os.path.isfile(PATH_WEIGHT) == False:
-    Link = "1KafnHz7ccT-3IyddBsL5yi2xGtxAKypt"
-    cmd = "gdown --id %s -O %s"%(Link, PATH_WEIGHT)
-    subprocess.call(cmd, shell=True, stdout=None)
 img_mean = np.array([104., 117., 123.])[:, np.newaxis, np.newaxis].astype('float32')
 
 
 class S3FD():
 
-    def __init__(self, device='cuda'):
+    def __init__(self, model_path, device='cuda'):
 
         tstamp = time.time()
         self.device = device
 
         # print('[S3FD] loading with', self.device)
         self.net = S3FDNet(device=self.device).to(self.device)
-        PATH = os.path.join(os.getcwd(), PATH_WEIGHT)
-        state_dict = torch.load(PATH, map_location=self.device)
+        state_dict = torch.load(model_path, map_location=self.device)
         self.net.load_state_dict(state_dict)
         self.net.eval()
         # print('[S3FD] finished loading (%.4f sec)' % (time.time() - tstamp))
